e9d69f8e9b9a1460193716ef468486c4b09d9641
Riley <riley@rileybathurst.com>
2022-02-23 12:34:12 -0800

    tracking ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,14 +5,14 @@ module.exports = {
   plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", {
     resolve: 'gatsby-plugin-google-analytics',
     options: {
-      "trackingId": ""
+      "trackingId": "G-PSZ3PWKQSC"
     }
   }, "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
-    },
-    __key: "images"
-  }]
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
+    }]
 };
