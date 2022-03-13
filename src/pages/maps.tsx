bf1a7531947932660570d4bdf718c3cacd398b6a
Riley <riley@rileybathurst.com>
2022-03-12 19:10:29 -0800

    node build

--- src/pages/maps.tsx
+++ src/pages/maps.tsx
@@ -5,91 +5,10 @@ import Header from "../components/header";
 import Footer from "../components/footer";
 
 // local build
-// import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
-
-import L from "leaflet";
-import { Marker, useMap } from "react-leaflet";
-
-import { promiseToFlyTo, getCurrentLocation } from "../lib/map";
-
-import Map from "../components/Map";
-
-const LOCATION = {
-  lat: 38.9072,
-  lng: -77.0369,
-};
-const CENTER = [LOCATION.lat, LOCATION.lng];
-const DEFAULT_ZOOM = 2;
-const ZOOM = 10;
-
-const timeToZoom = 2000;
-const timeToOpenPopupAfterZoom = 4000;
-const timeToUpdatePopupAfterZoom = timeToOpenPopupAfterZoom + 3000;
-
-const popupContentHello = `<p>Hello 👋</p>`;
-const popupContentGatsby = `
-  <div class="popup-gatsby">
-    <div class="popup-gatsby-content">
-      <h1>Gatsby Leaflet Starter</h1>
-      <p>Welcome to your new Gatsby site. Now go build something great!</p>
-    </div>
-  </div>
-`;
-
-/**
- * MapEffect
- * @description This is an example of creating an effect used to zoom in and set a popup on load
- */
-
-
-const MapEffect = ({ markerRef }) => {
-  const map = useMap();
-
-  useEffect(() => {
-    if (!markerRef.current || !map) return;
-
-    (async function run() {
-      const popup = L.popup({
-        maxWidth: 800,
-      });
-
-      const location = await getCurrentLocation().catch(() => LOCATION);
-
-      const { current: marker } = markerRef || {};
-
-      marker.setLatLng(location);
-      popup.setLatLng(location);
-      popup.setContent(popupContentHello);
-
-      setTimeout(async () => {
-        await promiseToFlyTo(map, {
-          zoom: ZOOM,
-          center: location,
-        });
-
-        marker.bindPopup(popup);
-
-        setTimeout(() => marker.openPopup(), timeToOpenPopupAfterZoom);
-        setTimeout(
-          () => marker.setPopupContent(popupContentGatsby),
-          timeToUpdatePopupAfterZoom
-        );
-      }, timeToZoom);
-    })();
-  }, [map, markerRef]);
-
-  return null;
-};
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
 // markup
 const MapsPage = () => {
-  const markerRef = useRef();
-
-  const mapSettings = {
-    center: CENTER,
-    defaultBaseMap: "OpenStreetMap",
-    zoom: DEFAULT_ZOOM,
-  };
 
   return (
     <>
@@ -98,7 +17,6 @@ const MapsPage = () => {
       <main className="measure">
 
         <h1>Maps</h1>
-        {/* only builds locally
         < MapContainer center={[51.505, -0.09]} zoom={13} >
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -109,12 +27,7 @@ const MapsPage = () => {
               A pretty CSS3 popup. <br /> Easily customizable.
             </Popup>
           </Marker>
-        </MapContainer > */}
-
-        <Map {...mapSettings}>
-          <MapEffect markerRef={markerRef} />
-          <Marker ref={markerRef} position={CENTER} />
-        </Map>
+        </MapContainer >
       </main>
 
       <Footer />
