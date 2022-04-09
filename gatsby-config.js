b6bf3372aecea6c268c84e53ec334b9faf78c5f5
Riley <riley@rileybathurst.com>
2022-04-08 17:54:06 -0700

    csp jobber form

--- gatsby-config.js
+++ gatsby-config.js
@@ -85,10 +85,11 @@ module.exports = {
         mergeScriptHashes: false, // you can disable scripts sha256 hashes
         mergeStyleHashes: false, // you can disable styles sha256 hashes
         directives: {
-          "script-src": "'self' 'unsafe-inline' www.google-analytics.com",
-          "style-src": "'self' 'unsafe-inline' use.typekit.net https://p.typekit.net/",
+          "script-src": "'self' 'unsafe-inline' www.google-analytics.com https://d3ey4dbjkt2f6s.cloudfront.net/",
+          "style-src": "'self' 'unsafe-inline' use.typekit.net https://p.typekit.net/ https://d3ey4dbjkt2f6s.cloudfront.net/",
           "font-src": "'self' 'unsafe-inline' use.typekit.net https://p.typekit.net/",
-          "img-src": "'self' data:"
+          "img-src": "'self' data:",
+          "connect-src": "'self' data:  https://www.google-analytics.com/"
           // you can add your directives or override defaults
         }
       }
