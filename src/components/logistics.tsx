aae268c1729d896969711df0ba9b303b15a177c5
Riley <riley@rileybathurst.com>
2022-06-03 13:17:56 -0700

    wedding links

--- src/components/logistics.tsx
+++ src/components/logistics.tsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { Link } from "gatsby"
 
 const Logistics = () => {
   return (
@@ -10,7 +11,7 @@ const Logistics = () => {
       <ul>
         <li key="provide">Provide you site plans, renderings, examples of past work</li>
         <li key="partner">Partner with you to design options to meet your budget and expectations</li>
-        <li key="send">Send out our crew of lighting technicians on your schedule to ensure your venue shines</li>
+        <li key="send">Send out our <Link to="/team">crew of lighting technicians</Link> on your schedule to ensure your venue shines</li>
         <li key="take">Take down everything and restore the site so it looks like we were never there</li>
       </ul>
       <hr />
