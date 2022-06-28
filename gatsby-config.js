1da839081f0dd9e3f7279a0ce263f2662a10d469
Riley <riley@rileybathurst.com>
2022-06-28 09:51:17 -0700

    add the tracking code to the head

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,113 +39,116 @@ module.exports = {
     itemType: 'LocalBusiness',
     priceRange: '$500-1500',
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", {
+  plugins: [{
     resolve: 'gatsby-plugin-google-analytics',
     options: {
-      "trackingId": "G-PSZ3PWKQSC"
+      "trackingId": "G-PSZ3PWKQSC",
+      head: true,
     }
   },
+    "gatsby-plugin-sass",
+    "gatsby-plugin-image",
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sitemap",
-    {
-      resolve: `gatsby-plugin-sharp`,
-      options: {
-        defaults: {
-          formats: [`auto`, `webp`],  //, this was off to get under a ram limit bug on netlify it seems to be working May '22
-          placeholder: `dominantColor`,
-          quality: 50,
-          breakpoints: [300, 750, 1080, 1366, 1920],
-          backgroundColor: `transparent`,
-          tracedSVGOptions: {},
-          blurredOptions: {},
-          jpgOptions: {},
-          pngOptions: {},
-          webpOptions: {},
-          avifOptions: {},
-        }
+  {
+    resolve: `gatsby-plugin-sharp`,
+    options: {
+      defaults: {
+        formats: [`auto`, `webp`],  //, this was off to get under a ram limit bug on netlify it seems to be working May '22
+        placeholder: `dominantColor`,
+        quality: 50,
+        breakpoints: [300, 750, 1080, 1366, 1920],
+        backgroundColor: `transparent`,
+        tracedSVGOptions: {},
+        blurredOptions: {},
+        jpgOptions: {},
+        pngOptions: {},
+        webpOptions: {},
+        avifOptions: {},
       }
-    },
+    }
+  },
     "gatsby-transformer-sharp", {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        "name": "images",
-        "path": "./src/images/"
-      },
-      __key: "images"
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
     },
+    __key: "images"
+  },
     'gatsby-plugin-netlify',
-    {
-      resolve: "gatsby-source-strapi",
-      options: strapiConfig,
-    },
-    {
-      resolve: `gatsby-plugin-csp`,
-      options: {
-        mergeSecurityHeaders: true, // ? testing june 18 2022 csp issue // boolean to turn off the default security headers
-        mergeScriptHashes: false, // you can disable scripts sha256 hashes
-        mergeStyleHashes: false, // you can disable styles sha256 hashes
-        directives: {
-          "script-src": "'self' 'unsafe-inline' use.typekit.net www.google-analytics.com https://d3ey4dbjkt2f6s.cloudfront.net/",
-          "style-src": "'self' 'unsafe-inline' use.typekit.net https://d3ey4dbjkt2f6s.cloudfront.net/",
-          "font-src": "'self' 'unsafe-inline' use.typekit.net data:",
-          "img-src": "'self' p.typekit.net https://www.google-analytics.com data: about:", // I think use.typekit.net is a tracking pixel
-          "connect-src": "'self' data:  https://www.google-analytics.com/"
-          // you can add your directives or override defaults
-        }
+  {
+    resolve: "gatsby-source-strapi",
+    options: strapiConfig,
+  },
+  {
+    resolve: `gatsby-plugin-csp`,
+    options: {
+      mergeSecurityHeaders: true, // ? testing june 18 2022 csp issue // boolean to turn off the default security headers
+      mergeScriptHashes: false, // you can disable scripts sha256 hashes
+      mergeStyleHashes: false, // you can disable styles sha256 hashes
+      directives: {
+        "script-src": "'self' 'unsafe-inline' use.typekit.net www.google-analytics.com https://d3ey4dbjkt2f6s.cloudfront.net/",
+        "style-src": "'self' 'unsafe-inline' use.typekit.net https://d3ey4dbjkt2f6s.cloudfront.net/",
+        "font-src": "'self' 'unsafe-inline' use.typekit.net data:",
+        "img-src": "'self' p.typekit.net https://www.google-analytics.com data: about:", // I think use.typekit.net is a tracking pixel
+        "connect-src": "'self' data:  https://www.google-analytics.com/"
+        // you can add your directives or override defaults
       }
+    }
+  },
+  {
+    // TODO
+    resolve: "gatsby-plugin-google-tagmanager",
+    options: {
+      id: "GTM-NFVF3W7 ",
+
+      // Include GTM in development.
+      //
+      // Defaults to false meaning GTM will only be loaded in production.
+      includeInDevelopment: false,
+
+      // datalayer to be set before GTM is loaded
+      // should be an object or a function that is executed in the browser
+      //
+      // Defaults to null
+      defaultDataLayer: { platform: "gatsby" },
+
+      // Specify optional GTM environment details.
+      gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+      gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+      dataLayerName: "YOUR_DATA_LAYER_NAME",
+
+      // Name of the event that is triggered
+      // on every Gatsby route change.
+      //
+      // Defaults to gatsby-route-change
+      routeChangeEventName: "YOUR_ROUTE_CHANGE_EVENT_NAME",
+      // Defaults to false
+      enableWebVitalsTracking: true,
+      // Defaults to https://www.googletagmanager.com
+      selfHostedOrigin: "YOUR_SELF_HOSTED_ORIGIN",
     },
-    {
-      // TODO
-      resolve: "gatsby-plugin-google-tagmanager",
-      options: {
-        id: "GTM-NFVF3W7 ",
-
-        // Include GTM in development.
-        //
-        // Defaults to false meaning GTM will only be loaded in production.
-        includeInDevelopment: false,
-
-        // datalayer to be set before GTM is loaded
-        // should be an object or a function that is executed in the browser
-        //
-        // Defaults to null
-        defaultDataLayer: { platform: "gatsby" },
-
-        // Specify optional GTM environment details.
-        gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
-        gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
-        dataLayerName: "YOUR_DATA_LAYER_NAME",
-
-        // Name of the event that is triggered
-        // on every Gatsby route change.
-        //
-        // Defaults to gatsby-route-change
-        routeChangeEventName: "YOUR_ROUTE_CHANGE_EVENT_NAME",
-        // Defaults to false
-        enableWebVitalsTracking: true,
-        // Defaults to https://www.googletagmanager.com
-        selfHostedOrigin: "YOUR_SELF_HOSTED_ORIGIN",
+  },
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `Sierra Lighting`,
+      short_name: `Sierra Lighting`,
+      start_url: `/`,
+      background_color: `#fffaf2`,
+      theme_color: `#fff`,
+      display: `standalone`,
+      icon: "src/images/sierra-lighting-icon.svg",
+    },
+  },
+  {
+    resolve: "gatsby-plugin-web-font-loader",
+    options: {
+      typekit: {
+        id: process.env.TYPEKIT_ID,
       },
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Sierra Lighting`,
-        short_name: `Sierra Lighting`,
-        start_url: `/`,
-        background_color: `#fffaf2`,
-        theme_color: `#fff`,
-        display: `standalone`,
-        icon: "src/images/sierra-lighting-icon.svg",
-      },
-    },
-    {
-      resolve: "gatsby-plugin-web-font-loader",
-      options: {
-        typekit: {
-          id: process.env.TYPEKIT_ID,
-        },
-      },
-    },
+  },
   ]
 };
