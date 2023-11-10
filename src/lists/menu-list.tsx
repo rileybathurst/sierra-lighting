38d367f0378d53353f9e2e7f3d0e02b31dc3e997
Riley <riley@rileybathurst.com>
2023-11-10 12:13:33 -0800

    small menu order

--- src/lists/menu-list.tsx
+++ src/lists/menu-list.tsx
@@ -1,13 +1,14 @@
 import React from "react"
 import { Link } from "gatsby"
+import Season from "../components/season"
 
 const MenuList = () => (
-  <ul>
-    <li key="wedding"><Link to="/wedding">Wedding Lighting</Link></li>
-    <li key="residential"><Link to="/residential">Residential Christmas Lights</Link></li>
-    <li key="commercial"><Link to="/commercial">Commercial Christmas Lights</Link></li>
-    <li key="contact"><Link to="/contact">Contact</Link></li>
-  </ul>
+  <Season>
+    <li key="wedding" className="wedding"><Link to="/wedding">Wedding Lighting</Link></li>
+    <li key="residential" className="xmas_r"><Link to="/residential">Residential Christmas Lights</Link></li>
+    <li key="commercial" className="xmas_c"><Link to="/commercial">Commercial Christmas Lights</Link></li>
+    <li key="contact" className="c"><Link to="/contact">Contact</Link></li>
+  </Season>
 )
 
 export default MenuList
