c72b559700afd198963cbce73e503d5bcad8c920
Riley <riley@rileybathurst.com>
2024-11-14 09:51:29 -0800

    oops took the description out

--- src/pages/work.tsx
+++ src/pages/work.tsx
@@ -105,6 +105,7 @@ export const Head = () => {
             "title": "${useStrapiJob().allStrapiJob.nodes[0].title}",
             "datePosted": "${useStrapiJob().allStrapiJob.nodes[0].updatedAt}",
             "employmentType": "${useStrapiJob().allStrapiJob.nodes[0].employmentType}",
+            "description": "${useStrapiJob().allStrapiJob.nodes[0].description.data.description.split('\n').join(' ')}",
             "validThrough": "${new Date(useStrapiJob().allStrapiJob.nodes[0].validThrough).toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', })}",
             "jobLocation": {
               "@type": "Place",
@@ -126,30 +127,3 @@ export const Head = () => {
     </SEO>
   )
 }
-
-//             "description": "${useStrapiJob().allStrapiJob.nodes[0].description.data.description.split('\n').join(' ')}",
-
-/* // TODO: schema.org/JobPosting
-                "baseSalary": "9000",
-                                "jobBenefits": "Medical, Life, Dental",
-"educationRequirements": "Bachelor's Degree in Computer Science, Information Systems or related fields of study.",
-"employmentType": "Full-time",
-"experienceRequirements": "Minumum 3 years experience as a software engineer",
-"incentiveCompensation": "Performance-based annual bonus plan, project-completion bonuses",
-"industry": "Computer Software",
-"jobLocation": {
-  "@type": "Place",
-  "address": {
-    "@type": "PostalAddress",
-    "addressLocality": "Kirkland",
-    "addressRegion": "WA"
-  }
-},
-"occupationalCategory": "15-1132.00 Software Developers, Application",
-"qualifications": "Ability to work in a team environment with members of varying skill levels. Highly motivated. Learns quickly.",
-"responsibilities": "Design and write specifications for tools for in-house customers Build tools according to specifications",
-"salaryCurrency": "USD",
-"skills": "Web application development using Java/J2EE Web application development using Python or familiarity with dynamic programming languages",
-"specialCommitments": "VeteranCommit",
-"title": "Software Engineer",
-"workHours": "40 hours per week" */
