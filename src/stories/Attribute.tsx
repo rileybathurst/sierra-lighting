3f1a3a5c97df6d1fb1ecae3bb15d7a76f9101cac
Riley Bathurst <riley@rileybathurst.com>
2026-04-30 14:58:41 -0700

    attributes few many work on storybook

--- src/stories/Attribute.tsx
+++ src/stories/Attribute.tsx
@@ -4,21 +4,39 @@ import { faker } from "@faker-js/faker";
 
 export const Attribute = () => {
   return (
-    <div className="attributes">
-      {Array.from({ length: faker.number.int({ min: 0, max: 12 }) }).map((_) => (
-        <section key={faker.number.int()} className="attribute">
-          <p className="key">{faker.music.genre()}</p>
-          {Array.from({ length: faker.number.int({ min: 1, max: 2 }) }).map(() => (
-            <h3 key={faker.number.int()} className="value">
-              <a href={faker.music.artist()}>
-                {faker.music.artist()}
-              </a>
-            </h3>
-          )
-          )}
-        </section>
-      ))}
-    </div>
+    faker.datatype.boolean() ? (
+      <div className="attributes attributes_few">
+        {Array.from({ length: faker.number.int({ min: 0, max: 4 }) }).map(() => (
+          <section key={faker.number.int()} className="attribute">
+            <p className="key">{faker.music.genre()}</p>
+            {Array.from({ length: faker.number.int({ min: 1, max: 2 }) }).map(() => (
+              <h3 key={faker.number.int()} className="value">
+                <a href={faker.music.artist()}>
+                  {faker.music.artist()}
+                </a>
+              </h3>
+            )
+            )}
+          </section>
+        ))}
+      </div>
+    ) : (
+      <div className="attributes attributes_many">
+        {Array.from({ length: faker.number.int({ min: 5, max: 12 }) }).map(() => (
+          <section key={faker.number.int()} className="attribute">
+            <p className="key">{faker.music.genre()}</p>
+            {Array.from({ length: faker.number.int({ min: 1, max: 2 }) }).map(() => (
+              <h3 key={faker.number.int()} className="value">
+                <a href={faker.music.artist()}>
+                  {faker.music.artist()}
+                </a>
+              </h3>
+            )
+            )}
+          </section>
+        ))}
+      </div>
+    )
   );
 };
 
